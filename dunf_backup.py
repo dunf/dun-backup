d5df39b8cc546c92d4b7927c96d62c8820cdda08
md <mihkal@dunfjeld.no>
2016-01-24 04:04:55 +0100

    ssh cockup

--- dunf_backup.py
+++ dunf_backup.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 
 __author__ = 'dunf'
-
+__version__ = "0.2.0"
 
 
 from os import path
@@ -10,9 +10,11 @@ from argparse import ArgumentParser
 from subprocess import call, check_output
 from time import strftime
 from timeit import default_timer
+import configparser
 
 # ---------------------- ARGPARSE ----------------------------------------------
 parser = ArgumentParser()
+
 mutually_exclusive = parser.add_mutually_exclusive_group()
 mutually_exclusive.add_argument("-f", "--full", help="Performs full backup with"
                                 "maximum compression", action="store_true")
@@ -21,12 +23,30 @@ mutually_exclusive.add_argument("-c", "--copy", help="Performs full backup with 
 mutually_exclusive.add_argument("-i", "--increment", help="Performs incremental backup",
                                 action="store_true")
 
+parser.add_argument("-d", '--destination', help="Manually specify destination",
+                    action="store_true")
+
+
+
 args = parser.parse_args()
 FULL_COMPRESSION = args.full
 NO_COMPRESSION = args.copy
 
 
+# ------------------------ CONFIG PARSER ---------------------------------------
+config = configparser.ConfigParser()
+
+def read_config():
+    try:
+        config.read('dunf_backup.cfg')
+    except configparser.ParsingError:
+        create_config()
+
+def create_config():
+    pass
 # ------------------------ GLOBAL VARIABLES ------------------------------------
+
+
 
 # ------------------------------------------------------------------------------
 
@@ -37,9 +57,8 @@ class Backup(object):
      # Current date and time, format: year_month_day__hour_minute
     current_time = strftime("%Y_%m_%d__%H_%M")
 
-    # Path to the file that contains paths to included in backupscript
+    # Path to the file that contains paths to included in backup script
     include_list = "/home/md/scripts/dunf_backup/dunf_include.conf"
-
     exclude_list = "/home/md/scripts/dunf_backup/dunf_exclude.conf"
 
 
@@ -58,44 +77,45 @@ class Backup(object):
                        no_compress_filename + " -ir@" + include_list + " -xr@" + exclude_list
 
 
-    # Incremental backup parameters
-    # Kommer i v0.2.0
-
     def check_dependency(self):
         if path.exists("/usr/bin/7za"):
             return True
         else:
-            call("sudo apt-get install p7zip-full")
+            print("Missing dependency p7zip-full")
+            call("sudo apt-get install p7zip-full", shell=True)
+
+
+
+    def check_destination(self):
+        if path.isdir(self.destination):
+            return True
+        else:
+            print("Destination not found...")
+            return False
 
     # Does a full backup using maximum compression
     def full_compression(self):
-        if path.isdir(self.destination):
-            start_time = default_timer()
-            call("7za" + self.full_compress_args, shell=True)
-            elapsed = default_timer() - start_time
-            print("Backup completed in ", elapsed.__round__(3), " seconds")
-        else:
-            print("Destination not found...")
-
+        start_time = default_timer()
+        call("7za" + self.full_compress_args, shell=True)
+        elapsed = default_timer() - start_time
+        print("Backup completed in ", elapsed.__round__(3), " seconds")
 
 
     # Does a full backup with no compression.
     def no_compression(self):
-        if path.isdir(self.destination):
-            start_time = default_timer()
-            call("7za" + self.no_compress_args, shell=True)
-            elapsed = default_timer() - start_time
-            print("Backup completed in ", elapsed.__round__(3), " seconds")
-        else:
-            print("Destination not found...")
+        start_time = default_timer()
+        call("7za" + self.no_compress_args, shell=True)
+        elapsed = default_timer() - start_time
+        print("Backup completed in ", elapsed.__round__(3), " seconds")
 
 
 
 if __name__ == "__main__":
     backup = Backup()
     backup.check_dependency()
+    backup.check_destination()
     if len(argv) < 2:
-        print("dunf BACKUP SCRIPT v0.1.2")
+        print("dunf BACKUP SCRIPT ", __version__)
         print("For help, use the '-h' or '--help' parameter")
     elif FULL_COMPRESSION:
         backup.full_compression()
