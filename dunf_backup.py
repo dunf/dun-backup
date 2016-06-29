3c467b1430854a84228bdcecc30eb70780204569
md <mihkal@dunfjeld.no>
2016-06-29 02:03:25 +0200

    In the process of merging include/exclude files into config

--- dunf_backup.py
+++ dunf_backup.py
@@ -7,6 +7,8 @@ from subprocess import call
 import configparser
 from time import strftime
 from timeit import default_timer
+from io import StringIO
+from io import BytesIO
 
 
 __author__ = 'Mihkal Dunfjeld'
@@ -34,13 +36,13 @@ def args():
 class Config(object):
     def __init__(self):
         """Initializes config object and reads config file."""
-        self.config = configparser.ConfigParser()
+        self._config = configparser.ConfigParser()
         self.read_config()
 
     def read_config(self):
         """Reads the config file."""
         try:
-            return self.config.read('./config.ini')
+            return self._config.read('./config.ini')
         except configparser.ParsingError:
             print("Config file not found...")
 
@@ -48,87 +50,103 @@ class Config(object):
         """Returns user specified destination directory if -d flag is set. If
         flag is not set default destination is fetched from config."""
         return args.destination[0] if args.destination else \
-            str(self.config.get('Default', option='destination'))
+            str(self._config.get('Default', option='destination'))
 
     def get_include_list(self):
         """Returns the path to the file containing paths to be included
         in backup."""
-        return self.config.get('Default', option='include_list')
+        return self._config.get('Default', option='include_list')
+
+    def include_list(self):
+        f = StringIO()
+        paths = []
+        for key, value in self._config.items('Include'):
+            f.write(str(value))
+        return f
 
     def get_exclude_list(self):
         """Returns the path to the file containing paths to be excluded
         in backup."""
-        return self.config.get('Default', option='exclude_list')
+        return self._config.get('Default', option='exclude_list')
 
 
 # --------------------------- Backup Class --------------------------------------
 class Backup(object):
-    config = Config()
+    def __init__(self, config = Config()):
+        self._config = config
 
     def include_list(self):
         """Returns path to include list"""
-        return self.config.get_include_list()
+        return self._config.include_list()
 
     def exclude_list(self):
         """Returns path to exclude list"""
-        return self.config.get_exclude_list()
+        return self._config.get_exclude_list()
 
     def get_destination(self):
         """Returns path to destination directory."""
-        return self.config.get_destination()
+        return self._config.get_destination()
 
     @staticmethod
     def get_filename():
         """Creates filename that is derived from time and type of compression."""
-        current_time = strftime("%Y_%m_%d__%H_%M")  # yyyy_mm_dd__hh_mm
+        timestamp = strftime("%Y_%m_%d__%H_%M")  # yyyy_mm_dd__hh_mm
         if args.full:
-            return "arch_backup_{}_full_compression.7z".format(current_time)
+            return "arch_backup_{}_full_compression.7z".format(timestamp)
         elif args.copy:
-            return "arch_backup_{}_no_compression.7z".format(current_time)
+            return "arch_backup_{}_no_compression.7z".format(timestamp)
         else:
             pass
 
-    def full_compression(self):
+    def full_compression(self, dest):
         """Performs backup with full compression."""
         start_time = default_timer()
         call("7za a -t7z -mx9 -mmt=on " + dest + self.get_filename() + " -ir@" \
              + self.include_list() + " -xr@" + self.exclude_list(), shell=True)
         elapsed = default_timer() - start_time
-        print("Saved to " + dest)
-        print("Backup completed in ", elapsed.__round__(3), " seconds")
+        print("Saved to {}...".format(dest))
+        print("Backup completed in {} seconds...".format(elapsed.__round__(3)))
 
-    def no_compression(self):
+    def no_compression(self, dest):
         """Performs backup with no compression."""
         start_time = default_timer()
-        call("7za a -t7z -mx0 " + dest + self.get_filename() + " -ir@" + \
+        call("7za a -ttar -mx0 " + dest + self.get_filename() + " -ir@" + \
              self.include_list() + " -xr@" + self.exclude_list(),  shell=True)
         elapsed = default_timer() - start_time
-        print("Saved to {}".format(dest))
-        print("Backup completed in {} seconds".format(elapsed.__round__(3)))
+        print("Saved to {}...".format(dest))
+        print("Backup completed in {} seconds...".format(elapsed.__round__(3)))
 
     def incremental_backup(self, dest, file_name):
         raise NotImplementedError
         pass
-# ------------------------ GLOBAL VARIABLES  AND FUNCTIONS ----------------------
+
+
+# Should I continue with this or just abandon the idea?
 def check_dependency():
     """Checks if 7-zip is installed and asks the user """
     if not path.exists("/usr/bin/7za"):
         print("Missing dependency p7zip-full")
         call("sudo pacman -S p7zip", shell=True)
 # ------------------------------------ MAIN -------------------------------------
+def main():
+    """Main entry point"""
 
-if __name__ == "__main__":
-    args = args()
+   # args = args()
     backup = Backup()
-    dest = backup.config.get_destination()
+    dest = backup.get_destination()
     if len(sys.argv) < 2:
         print("dunf BACKUP SCRIPT {}".format(__version__))
         print("For help, use the '-h' or '--help' parameter")
     elif args.full:
-        sys.exit(backup.full_compression())
+        backup.full_compression()
     elif args.copy:
-        sys.exit(backup.no_compression())
+        print(backup.include_list())
+        backup.no_compression(dest)
     elif args.increment:
-        sys.exit(backup.incremental_backup())
+        backup.incremental_backup()
     else:
         pass
+
+if __name__ == "__main__":
+    args = args()
+    sys.exit(main())
