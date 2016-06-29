de2dfd06f80eb78240b8449f47dc6f32cc340bbb
md <mihkal@dunfjeld.no>
2016-06-29 18:27:09 +0200

    Update: switched archiver from 7z to tar and finished merging exclude/include into config file.

--- dunf_backup.py
+++ dunf_backup.py
@@ -7,42 +7,35 @@ from subprocess import call
 import configparser
 from time import strftime
 from timeit import default_timer
-from io import StringIO
-from io import BytesIO
-
 
 __author__ = 'Mihkal Dunfjeld'
-__version__ = "0.2.3"
+__version__ = "1.0.0"
 
 
-
-
-# ---------------------- ARGPARSE ----------------------------------------------
 def args():
     parser = ArgumentParser()
     mu = parser.add_mutually_exclusive_group()
     mu.add_argument("-f", "--full", help="Performs full backup with"
-                                    "maximum compression", action="store_true")
+                    "maximum compression", action="store_true")
     mu.add_argument("-c", "--copy", help="Performs full backup with "
-                                    "no compression", action="store_true")
+                    "no compression", action="store_true")
     mu.add_argument("-i", "--increment", help="Performs incremental backup",
-                                    action="store_true")
+                    action="store_true")
     parser.add_argument("-d", "--destination", nargs=1,
                         help="Manually specify destination")
     return parser.parse_args()
 
 
-# ------------------------ CONFIG PARSER ----------------------------------------
 class Config(object):
-    def __init__(self):
+    def __init__(self, config=configparser.ConfigParser()):
         """Initializes config object and reads config file."""
-        self._config = configparser.ConfigParser()
+        self._config = config
         self.read_config()
 
     def read_config(self):
         """Reads the config file."""
         try:
-            return self._config.read('./config.ini')
+            return self._config.read('./dunf_backup.ini')
         except configparser.ParsingError:
             print("Config file not found...")
 
@@ -52,27 +45,25 @@ class Config(object):
         return args.destination[0] if args.destination else \
             str(self._config.get('Default', option='destination'))
 
-    def get_include_list(self):
-        """Returns the path to the file containing paths to be included
-        in backup."""
-        return self._config.get('Default', option='include_list')
-
     def include_list(self):
-        f = StringIO()
+        """Fetches all include paths from dunf_backup.ini and returns them
+        as a single space-separeted string."""
         paths = []
         for key, value in self._config.items('Include'):
-            f.write(str(value))
-        return f
+            paths.append(value)
+        return ' '.join(paths)
 
-    def get_exclude_list(self):
-        """Returns the path to the file containing paths to be excluded
-        in backup."""
-        return self._config.get('Default', option='exclude_list')
+    def exclude_list(self):
+        """Fetches all exclude paths from dunf_backup.ini and returns them
+        as a single string."""
+        paths = []
+        for key, value in self._config.items('Exclude'):
+            paths.append(value)
+        return ' --exclude='.join(paths)
 
 
-# --------------------------- Backup Class --------------------------------------
 class Backup(object):
-    def __init__(self, config = Config()):
+    def __init__(self, config=Config()):
         self._config = config
 
     def include_list(self):
@@ -81,7 +72,7 @@ class Backup(object):
 
     def exclude_list(self):
         """Returns path to exclude list"""
-        return self._config.get_exclude_list()
+        return self._config.exclude_list()
 
     def get_destination(self):
         """Returns path to destination directory."""
@@ -94,31 +85,30 @@ class Backup(object):
         if args.full:
             return "arch_backup_{}_full_compression.7z".format(timestamp)
         elif args.copy:
-            return "arch_backup_{}_no_compression.7z".format(timestamp)
+            return "arch_backup_{}.tar.gz".format(timestamp)
         else:
             pass
 
     def full_compression(self, dest):
         """Performs backup with full compression."""
         start_time = default_timer()
-        call("7za a -t7z -mx9 -mmt=on " + dest + self.get_filename() + " -ir@" \
-             + self.include_list() + " -xr@" + self.exclude_list(), shell=True)
+        call("7za a -t7z -mx9 -mmt=on " + dest + self.get_filename() +
+             self.include_list() + " -xr@" + self.exclude_list(), shell=True)
         elapsed = default_timer() - start_time
         print("Saved to {}...".format(dest))
         print("Backup completed in {} seconds...".format(elapsed.__round__(3)))
 
-    def no_compression(self, dest):
+    def copy_files(self, dest):
         """Performs backup with no compression."""
         start_time = default_timer()
-        call("7za a -ttar -mx0 " + dest + self.get_filename() + " -ir@" + \
-             self.include_list() + " -xr@" + self.exclude_list(),  shell=True)
+        call("tar --exclude " + self.exclude_list() + " -cvf " + dest +
+             self.get_filename() + " " + self.include_list(), shell=True)
         elapsed = default_timer() - start_time
         print("Saved to {}...".format(dest))
         print("Backup completed in {} seconds...".format(elapsed.__round__(3)))
 
-    def incremental_backup(self, dest, file_name):
+    def incremental_backup(self, dest):
         raise NotImplementedError
-        pass
 
 
 # Should I continue with this or just abandon the idea?
@@ -127,25 +117,24 @@ def check_dependency():
     if not path.exists("/usr/bin/7za"):
         print("Missing dependency p7zip-full")
         call("sudo pacman -S p7zip", shell=True)
-# ------------------------------------ MAIN -------------------------------------
+
+
 def main():
     """Main entry point"""
-
-   # args = args()
     backup = Backup()
     dest = backup.get_destination()
     if len(sys.argv) < 2:
         print("dunf BACKUP SCRIPT {}".format(__version__))
         print("For help, use the '-h' or '--help' parameter")
     elif args.full:
-        backup.full_compression()
+        backup.full_compression(dest)
     elif args.copy:
-        print(backup.include_list())
-        backup.no_compression(dest)
+        backup.copy_files(dest)
     elif args.increment:
-        backup.incremental_backup()
+        backup.incremental_backup(dest)
     else:
         pass
+
 
 if __name__ == "__main__":
     args = args()
