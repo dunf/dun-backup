ef5220829df8a6d0bfe52b592ae5e2690849727e
md <mihkal@dunfjeld.no>
2016-07-07 02:50:51 +0200

    Update: added argument for passing config files

--- dunf_backup.py
+++ dunf_backup.py
@@ -9,37 +9,65 @@ from time import strftime
 from timeit import default_timer
 
 __author__ = 'Mihkal Dunfjeld'
-__version__ = "1.0.0"
+__version__ = "1.1.0"
+
+
+def Args():
+    parser = ArgumentParser()
+    mu = parser.add_mutually_exclusive_group()
+    parser.add_argument("-t", "--type", choices=['i', 'f'], default='f',
+                        help='Specifies backup type.'
+                             ' (i)ncremental or (f)ull backup.')
+    parser.add_argument("-c", "--compression",
+                        help="Whether to compress the files"
+                        "or not.", action="store_true")
+    parser.add_argument("-d", "--destination", nargs=1,
+                        help="Manually specify destination")
+    parser.add_argument("-C", "--config", nargs=1, help="Specify path to config"
+                                                        "file.")
+    return parser.parse_args()
 
 
 class Config(object):
+    args = Args()
+
     def __init__(self, config=configparser.ConfigParser()):
         """Initializes config object and reads config file."""
         self._config = config
+        print(self.args)
         self.read_config()
 
     def read_config(self):
         """Reads the config file."""
-        try:
-            return self._config.read(path.join(environ['HOME'],
-                                               '.dunf_backup.ini'))
-        except configparser.ParsingError:
-            print("Config file not found...")
-            self.create_config()
+        if self.args.config is None:
+            try:
+                return self._config.read(path.join(environ['HOME'],
+                                                   '.dunf_backup.ini'))
+            except configparser.ParsingError:
+                print("Config file not found. Creating config file "
+                      "{}/.dunf_backup.ini.".format(environ['HOME']))
+                self.create_config()
+                sys.exit(0)
+        else:  # If config is user specified
+            if path.exists(self.args.config[0]):
+                return self._config.read(self.args.config[0])
+            else:
+                print("Config file does not exist...")
+                sys.exit(1)
 
     def create_config(self):
         """Creates INI file with appropriate sections and default values
-        if it does not exist."""
+        if INI file does not exist."""
         raise NotImplementedError
 
     def get_destination(self):
         """Returns user specified destination directory if -d flag is set. If
         flag is not set default destination is fetched from config."""
-        return args.destination[0] if args.destination else \
+        return self.args.destination[0] if self.args.destination else \
             str(self._config.get('Default', option='destination'))
 
     def include_list(self):
-        """Fetches all include paths from dunf_backup.ini and returns them
+        """Fetches all include paths from config file and returns them
         as a single space-separated string."""
         paths = []
         for key, value in self._config.items('Include'):
@@ -64,6 +92,9 @@ class Backup(object):
         self._include_list = self._config.include_list()
         self._exclude_list = self._config.exclude_list()
 
+    def get_args(self):
+        return self._config.args
+
     def get_destination(self):
         """Returns path destination directory."""
         return self._destination
@@ -72,80 +103,38 @@ class Backup(object):
     def get_filename():
         """Creates filename that is derived from time and type of compression."""
         timestamp = strftime("%Y_%m_%d__%H_%M")  # yyyy_mm_dd__hh_MM
-        if args.full:
-            return "arch_backup_{}_full_compression.7z".format(timestamp)
-        elif args.copy:
-            return "arch_backup_{}.tar.gz".format(timestamp)
+        if Config.args.compression:
+            return "arch_backup_{}_compressed.tar.gz".format(timestamp)
         else:
-            pass
+            return "arch_backup_{}.tar.gz".format(timestamp)
 
-# Probably won't work at the moment.
-    def full_compression(self, dst):
-        """Performs backup with full compression."""
+    def run_backup(self, destination, compression):
+        """Runs the backup script..."""
+        tar_option = ' -czvf' if compression else ' -cvf'
         start_time = default_timer()
-        call("7za a -t7z -mx9 -mmt=on " + dst + self.get_filename() +
-             self._include_list + " -xr@" + self._exclude_list, shell=True)
+        call("tar --exclude={a} {b} {c}{d} {e}".format(
+             a=self._exclude_list,
+             b=tar_option,
+             c=destination,
+             d=self.get_filename(),
+             e=self._include_list), shell=True)
         elapsed = default_timer() - start_time
-        print("Saved to {}...".format(dst))
+        print("Saved to {}...".format(destination))
         print("Backup completed in {} seconds...".format(elapsed.__round__(3)))
-
-    def copy_files(self, dst):
-        """Performs backup with no compression."""
-        start_time = default_timer()
-        call("tar --exclude " +
-             self._exclude_list +
-             " -cvf " +
-             dst +
-             self.get_filename() +
-             " " +
-             self._include_list, shell=True)
-        elapsed = default_timer() - start_time
-        print("Saved to {}...".format(dst))
-        print("Backup completed in {} seconds...".format(elapsed.__round__(3)))
-
-    def incremental_backup(self, dst):
-        raise NotImplementedError
-
-
-# Should I continue with this or just abandon the idea?
-def check_dependency():
-    """Checks if 7zip is installed and asks the user to install it if its not."""
-    if not path.exists("/usr/bin/7za"):
-        print("Missing dependency p7zip-full")
-        call("sudo pacman -S p7zip", shell=True)
-
-
-def args():
-    parser = ArgumentParser()
-    mu = parser.add_mutually_exclusive_group()
-    mu.add_argument("-f", "--full", help="Performs full backup with"
-                    "maximum compression", action="store_true")
-    mu.add_argument("-c", "--copy", help="Copy files to archive with no "
-                    "compression", action="store_true")
-    mu.add_argument("-i", "--increment", help="Performs incremental backup",
-                    action="store_true")
-    parser.add_argument("-d", "--destination", nargs=1,
-                        help="Manually specify destination")
-    return parser.parse_args()
 
 
 def main():
     """Main entry point"""
     backup = Backup()
     dst = backup.get_destination()
-    if len(sys.argv) < 2:
-        print("dunf BACKUP {}".format(__version__))
-        print("For help, use the '-h' or '--help' parameter")
-    elif args.full:
-        backup.full_compression(dst)
-    elif args.copy:
-        backup.copy_files(dst)
-    elif args.increment:
-        backup.incremental_backup(dst)
-    else:
+    a = backup.get_args()
+    compression = a.compression
+    if a.type == 'f':
+        backup.run_backup(dst, compression)
         pass
+    elif a.type == 'i':
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
-    args = args()
     sys.exit(main())
